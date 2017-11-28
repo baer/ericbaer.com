bcf331ec5a5931bfcf505a4844de7bb2728be795
Eric Baer <me@ericbaer.com>
2017-11-28 17:51:44 -0600

    Small styling updates

--- src/pages/graphql/index.jsx
+++ src/pages/graphql/index.jsx
@@ -5,6 +5,8 @@ import "./style.scss";
 const Profile = () => {
   return (
     <div className="container profileMainContent">
+      <h1 className="text-center">A List of GraphQL Resources</h1>
+
       <main role="main">
         <div className="jumbotron">
           <p className="text-center">
