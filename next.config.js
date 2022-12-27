3b3ef3278675e4afbf907a2bc5e6fee9846ae3a5
Eric Baer <me@ericbaer.com>
2022-12-27 11:34:04 -0800

    Upgrade Next.js 12=>13

--- next.config.js
+++ next.config.js
@@ -2,9 +2,7 @@ module.exports = {
   reactStrictMode: true,
   // Since the site is exposted statically, the fancy Next.js image opts need
   // to be turned off.
-  experimental: {
-    images: {
-      unoptimized: true,
-    },
+  images: {
+    unoptimized: true,
   },
 };
